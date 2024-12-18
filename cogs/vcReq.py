65e456a2528a44190bbf51e6994c660626a1885b
nixie <122477595+dual-shock@users.noreply.github.com>
2024-12-18 16:08:05 +0100

    made /req command

--- cogs/vcReq.py
+++ cogs/vcReq.py
@@ -1,31 +1,123 @@
-import interactions
-#import importlib
 import asyncio
+from interactions import (
+    Extension, 
+    slash_command, 
+    SlashContext,
+    listen,
+    OptionType, 
+    slash_option,
+    Member,ActionRow, Button, ButtonStyle, )
+from interactions.api.events.discord import (
+    VoiceStateUpdate
+)
+from interactions.api.events import Component
 
-class vcReq(interactions.Extension):
+
+
+
+
+
+
+class vcReq(Extension):
     def __init__(self, client):
         self.client = client
-        print(self.extension_checks)
         asyncio.create_task(self.async_init())
+        self.dev_channel = self.client.get_channel(1318190826458714214)
     def drop(self):
         asyncio.create_task(self.async_drop())
         super().drop()
-
-    # doing something when the cog gets loaded
     async def async_init(self):
-        print(f"- {self.__class__.__name__} loaded!")
-        
-        vc = self.bot.get_channel(1233254546340581416)
-        print(vc.members)
-        
-
-    # # doing something when the cog gets unloaded
+        print(f"- {self.__class__.__name__} loaded!")    
     async def async_drop(self):
         print(f"- {self.__class__.__name__} unloaded!")
-        
         pass
 
 
+    @listen(VoiceStateUpdate)
+    async def on_voice_state_update(self, event: VoiceStateUpdate):
+        print(event.before, event.after)
+        if event.before is not None: 
+            channel_left = event.before.channel.name
+            member_left = event.before.member.mention
+            await self.dev_channel.send(f"GLOBAL VoiceState update, {member_left} left {channel_left}")
+        if event.after is not None: 
+            channel_left = event.after.channel.name
+            member_left = event.after.member.mention
+            await self.dev_channel.send(f"GLOBAL VoiceState update, {member_left} joined {channel_left}")
+        return event
+            
+    @slash_command(
+        name="req", 
+        description="request to join someones vc or request for someone to join your vc"
+    )
+    @slash_option(
+        name="user", 
+        description="the member you're requesting to join",
+        required=True,
+        opt_type=OptionType.USER
+    )
+    async def req(self, ctx: SlashContext, user: Member):
+        SENDER = ctx.author
+        RECIPIENT = user
+        # if recipient of the request is in a voice channel
+        if RECIPIENT.voice is not None:
+            SENDER_REQUESTED_VC = RECIPIENT.voice.channel
+
+            req_recipient_actrow = ActionRow(
+                Button(
+                    style=ButtonStyle.GREEN,
+                    label="Accept request",
+                ),
+                Button(
+                    style=ButtonStyle.RED,
+                    label="Deny request",
+                )
+            )
+            components: list[ActionRow] = [req_recipient_actrow]
+            # request RECIPIENT if they allow author to join, valid for 5 minutes, see used_component
+        
+            no_mic_channel = self.client.get_channel(1279254041662197820)
+            req_recipient_message = await ctx.send(f"{SENDER.mention} is requesting {RECIPIENT.mention} to join {SENDER_REQUESTED_VC.name}", components=components, ephemeral=False)
+            
+            # persisent check to make sure only recipient can answer the request, and create an invite
+            async def check(component: Component) -> bool:
+                if component.ctx.user.id == RECIPIENT.id: 
+                    await self.dev_channel.send("the component interactor and request recipient are the same")
+                    return True
+                else:
+                    await self.dev_channel.send("the component interactor and request recipient are NOT the same")
+                    await component.ctx.send(f"Sorry, you're not the recipient of this request. {component.ctx.user} is.", ephemeral=True)
+                    return False 
+                
+            try: used_component: Component = await self.client.wait_for_component(components = req_recipient_actrow,check=check, timeout = 60*5)
+            except: await req_recipient_message.edit(components=components, content="Request timed out")
+            else: 
+                if used_component.ctx.component.label == "Accept request":
+                    await self.dev_channel.send("the request to join was ACCEPTED by the RECIPIENT")
+                    if SENDER.voice is not None: #if SENDER is in a vc
+                        await self.dev_channel.send("the REQUESTOR is in a vc, and is getting moved")
+                        await SENDER.move(SENDER_REQUESTED_VC.id)
+                        await req_recipient_message.edit(components=[], content=f"The request has been accepted. Moving the requestor to the recipients channel.")
+                    else: #if SENDER is not in a vc
+                        await req_recipient_message.edit(components=[], content=f"The request has been accepted. Waiting for 5 minutes to see if the requestor joins.")
+                        await self.dev_channel.send("the REQUESTOR is NOT in a vc, and is NOT getting moved")
+                        await self.dev_channel.send("waiting for 5 minutes to see if they join a vc")
+                        try: 
+                            await self.client.wait_for("on_voice_state_update", timeout=60*5)
+                            await self.dev_channel.send("voice state event detected by waiter")
+                            await SENDER.move(SENDER_REQUESTED_VC.id)
+                        except TimeoutError: 
+                            await self.dev_channel.send("the REQUESTOR did not join a vc in time")
+                else: #used_component.ctx.component.label == "Deny request":
+                    await self.dev_channel.send("the request to join was DENIED by the RECIPIENT")
+                    await ctx.send("You quietly denied the request, the denied user will not be notified.", ephemeral=True)
+                    await req_recipient_message.delete()
+                    
+
+        else: # if recipient of the request is not in a voice channel
+            await ctx.send("The person you're trying to request is not in a voice channel", ephemeral=True) 
+        
+
+
 def setup(client):
-    # finally, adding the cog to the bot
     vcReq(client)
--- main.py
+++ main.py
@@ -1,45 +1,33 @@
-#import discord
-#from discord.ext import commands 
 import dotenv
 import os
 from pathlib import Path
 
-from interactions import Client, listen, Intents
-import interactions.ext.prefixed_commands as pre
-
-
-
-#X:/Desktop/repos/sillybot/.venv/Scripts/python.exe -m pip install discord-py-interactions --upgrade
-
-
+from interactions import (
+    Client, 
+    listen, 
+    Intents, 
+    slash_command,
+    SlashContext,
+    check,
+    is_owner)
 
 dotenv.load_dotenv(Path(__file__).with_name("bot-token.env"))
 BOT_TOKEN = os.getenv("DISCORD_TOKEN")
-
-#intents = discord.Intents.default()
-#intents.message_content = True
-
 class Silly(Client):
     def __init__(self):
-        super().__init__(
-            # command_prefix="",
-            # case_insensetive=True,
-            intents=Intents.ALL
-        )
+        super().__init__(intents=Intents.ALL)
         self.cogs_list = [
             #'game_commands',
             #'events'
             'vcReq'
         ]
     
-
     def load_cogs(self):
         print('\nLoading extention cogs!')
         for filename in os.listdir(Path(__file__).parent / "./cogs"):
             if filename[:-3] in self.cogs_list:
                 print(f'- Loading cogs.{filename[:-3]}')
                 self.load_extension(f"cogs.{filename[:-3]}")
-        print(f'Loaded extention cogs!\n')
 
     def reload_cogs(self):
         print('\nReloading extension cogs!')
@@ -47,26 +35,27 @@ class Silly(Client):
             if filename[:-3] in self.cogs_list:
                 print(f"- Reloading cogs.{filename[:-3]}!")
                 self.reload_extension(f"cogs.{filename[:-3]}")
-        print(f'Reloaded extention cogs!\n')
-
-
 
 Silly = Silly()
-pre.setup(Silly, default_prefix="-")
 
 @listen()
 async def on_ready():
+    print(f'Silly activated as {Silly.user}! loading cogs now...')
     Silly.load_cogs()
-    print(f'Silly activated as {Silly.user}')
     
 
-@pre.prefixed_command(name='ReloadCogs')
-async def _reload_cogs(ctx: pre.PrefixedContext):
-    if ctx.author != Silly.owner:
-        await ctx.send("You're not Silly's owner...")
-    else:
-        Silly.reload_cogs()
-        await ctx.send("Reloaded cogs Nora<3")
+@slash_command(name="reload_cogs",
+               description="Reloads the bot without restarting")
+@check(is_owner())
+async def reload_cogs(ctx: SlashContext):
+    Silly.reload_cogs()
+    await ctx.send("Reloaded cogs Nora<3", ephemeral=True)
+@reload_cogs.error
+async def req_error(ctx: SlashContext, error: Exception):
+    pass
+    #await ctx.send(error, "\n\nYou're not Silly's owner...")
+
+
 
 Silly.start(BOT_TOKEN)
     #log_handler=logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
